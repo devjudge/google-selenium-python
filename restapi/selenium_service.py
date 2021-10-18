9ede7587a1ac053cc4d1a6c05540fa9ee3ddaa05
Simar Singh <simar@codejudge.io>
2021-10-18 14:37:25 +0530

    template for google title question

---
+++ restapi/selenium_service.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class SeleniumService(object):
+
+    @classmethod
+    def get_title_from_google(cls):
+        """Complete the method to return browser title"""
+        pass
--- restapi/views.py
+++ restapi/views.py
@@ -1,11 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import logging
 
 from django.http import HttpResponse
-# from django.shortcuts import render
+from rest_framework import status
 
-# Create your views here.
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+
+from restapi.selenium_service import SeleniumService
+
+logger = logging.getLogger(__name__)
 
 
 def index():
     return HttpResponse("Hello, world. You're at Rest.")
+
+
+@api_view(['GET'])
+@renderer_classes([JSONRenderer, ])
+def get_title(request, format=None):
+    """A view that returns google title"""
+    title = SeleniumService.get_title_from_google()
+    return Response({'response': title}, status=status.HTTP_200_OK)
